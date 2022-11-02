ddcb73054ede1375c3efeaa3f536b1f0dd758ad5
Saulo Klein Nery <saulokn@outlook.com>
2022-11-01 22:23:50 -0300

    feat(server): Create get's that counts users and guesses, and a post to create a pool

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
+import { z } from 'zod';
+import ShortUniqueId from 'short-unique-id';
 
 const prisma = new PrismaClient({
     log: ['query'],
@@ -19,6 +21,39 @@ async function bootstrap() {
         const countPools = await prisma.pool.count();
 
         return { countPools };
+    });
+
+    fastify.get('/guesses/count', async () => {
+        const countGuesses = await prisma.guess.count();
+
+        return { countGuesses };
+    });
+
+    fastify.get('/user/count', async () => {
+        const countUsers = await prisma.user.count();
+
+        return { countUsers };
+    });
+
+    fastify.post('/pools', async (request, reply) => {
+        const createPoolBody = z.object({
+            title: z.string(),
+        });
+
+        const { title } = createPoolBody.parse(request.body);
+
+        const generate = new ShortUniqueId({ length: 6 });
+
+        const code = String(generate()).toUpperCase();
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code
+            }
+        })       
+
+        return reply.status(201).send({ code });
     });
 
 
